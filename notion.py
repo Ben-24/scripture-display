bf5704b9da2556d54cde5c153a82474f814f2fe2
ben-24 <benjaminlmerrell@gmail.com>
2024-06-13 20:46:43 -0600

    notion and screen classes

---
+++ notion.py
@@ -0,0 +1,65 @@
+import network # type: ignore
+import time
+import urequests # type: ignore # handles making and servicing network requests
+import json
+
+class Notion():
+
+    def __init__(self, wifi_parameters, notion_database_id, notion_auth):
+        # Set parameters
+        self.ssid = wifi_parameters["ssid"] 
+        self.password = wifi_parameters["password"]
+        self.database_id = notion_database_id
+        self.auth = notion_auth
+        self.wlan = network.WLAN(network.STA_IF)
+        
+        self.connect_to_wifi()
+    
+    def __del__(self):
+        # Disconnect from the internet upon deleting the object
+        self.disconnect_from_wifi()
+    
+    def connect_to_wifi(self):
+        # Connect to the WIFI
+        print("Connecting to WiFi", end="")
+        self.wlan.active(True)
+        self.wlan.connect(self.ssid, self.password)
+        while not self.wlan.isconnected():
+            print(".", end="")
+            time.sleep(0.1)
+        print(" Connected!")
+        print(self.wlan.ifconfig())
+        
+    def disconnect_from_wifi(self):
+        # Disconnect from the internet
+        self.wlan.disconnect()
+        time.sleep(1)
+        if not self.wlan.isconnected():
+            print("Disconnected from WiFi")
+        else:
+            print("Failed to disconnect from WiFi")
+
+    def get_time(self):
+        response = urequests.get("https://worldtimeapi.org/api/timezone/America/Denver")
+        data = response.json()
+        # print(data)
+        timestamp = data["datetime"]
+        print(timestamp)
+        return timestamp
+
+    def get_daily_goal(self):
+        headers = { 'Authorization' : f'Bearer {self.auth}',
+                    'Notion-Version' : '2022-06-28',
+                    'Content-Type': 'application/json',
+                    }
+        data = {"filter": {
+                    "property": "Date",
+                    "rich_text": {
+                        "contains": f"2024-06-11",
+                    },
+                }}
+        response = urequests.post(f"https://api.notion.com/v1/databases/{self.database_id}/query", headers=headers, data=json.dumps(data))
+        text = response.text
+        response.close()
+        return text
+    #['results'][0]['properties']['Name']['title'][0]['plain_text']
--- screen.py
+++ screen.py
@@ -6,7 +6,10 @@ class Screen():
     def __init__(self):
         i2c=I2C(0,sda=Pin(0), scl=Pin(1), freq=400000)
 
-        oled = SSD1306_I2C(128, 32, i2c)
+        self.oled = SSD1306_I2C(128, 32, i2c)
+        
+        self.oled.text("Dude", 0, 0)
 
-        oled.text('Scrip: 1Cor 13:1-3', 0, 0)
-        oled.show()
+    def write_text(self, text, x_locaiton, y_location):
+        self.oled.text(text, x_locaiton, y_location)
+        self.oled.show()
